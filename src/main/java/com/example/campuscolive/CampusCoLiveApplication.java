eb206521a8da2730f152d5f6648f048e884211cb
Matthew Caudill <caudill.matthew0127@gmail.com>
2025-04-07 01:32:19 -0500

    -Connected MongoDB database -Working through CRUD operations

--- src/main/java/com/example/campuscolive/CampusCoLiveApplication.java
+++ src/main/java/com/example/campuscolive/CampusCoLiveApplication.java
@@ -1,13 +1,30 @@
 package com.example.campuscolive;
 
+import com.example.campuscolive.entity.UserMongoDB;
+import com.example.campuscolive.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
-public class CampusCoLiveApplication {
+@EnableMongoRepositories
+public class CampusCoLiveApplication implements CommandLineRunner {
 
+	@Autowired
+	UserRepository userRepo;
+
+	List<UserMongoDB> userMongoDBList = new ArrayList<UserMongoDB>();
 	public static void main(String[] args) {
 		SpringApplication.run(CampusCoLiveApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
